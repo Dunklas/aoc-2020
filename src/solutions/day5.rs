1fa6b9f1e0666ddac17d1b63025726f3d6644223
Rickard Andersson <dunklas@gmail.com>
2020-12-05 13:45:49 +0100

    Better solution for day 5!

--- src/solutions/day5.rs
+++ src/solutions/day5.rs
@@ -1,5 +1,3 @@
-use std::{cmp};
-
 pub fn run(input: String) {
     println!("Part 1: {:?}", part_1(&input));
     println!("Part 2: {:?}", part_2(&input));
@@ -7,16 +5,14 @@ pub fn run(input: String) {
 
 fn part_1(input: &String) -> usize {
     input.lines()
-        .map(|line| seat_to_row_and_col(line))
-        .map(|row_and_col| row_and_col.0 * 8 + row_and_col.1)
+        .map(|line| seat_to_id(line))
         .max()
         .unwrap()
 }
 
 fn part_2(input: &String) -> usize {
     let mut passes: Vec<usize> = input.lines()
-        .map(|line| seat_to_row_and_col(line))
-        .map(|row_and_col| row_and_col.0 * 8 + row_and_col.1)
+        .map(|line| seat_to_id(line))
         .collect();
     passes.sort();
     for i in 1..passes.len() {
@@ -27,21 +23,11 @@ fn part_2(input: &String) -> usize {
     return 0;
 }
 
-fn seat_to_row_and_col(seat: &str) -> (usize, usize) {
-    let mut row: (usize, usize) = (0, 127);
-    let mut col: (usize, usize) = (0, 7);
-    for c in seat.chars() {
-        let row_diff = cmp::max(row.0, row.1) - cmp::min(row.0, row.1) + 1;
-        let col_diff = cmp::max(col.0, col.1) - cmp::min(col.0, col.1) + 1;
-        match c {
-            'F' => row = (row.0, row.1 - (row_diff / 2)),
-            'B' => row = (row.0 + row_diff / 2, row.1),
-            'L' => col = (col.0, col.1 - (col_diff / 2)), 
-            'R' => col = (col.0 + col_diff / 2, col.1),
-            _ => {}
-        }
-    }
-    (row.0, col.0)
+fn seat_to_id(seat: &str) -> usize {
+    usize::from_str_radix(
+        &seat.replace("R", "1").replace("B", "1").replace("L", "0").replace("F", "0"),
+        2
+    ).unwrap()
 }
 
 #[cfg(test)]
@@ -50,9 +36,9 @@ mod tests {
 
     #[test]
     fn seat_to_row_and_col_test() {
-        assert_eq!(seat_to_row_and_col("FBFBBFFRLR"), (44, 5));
-        assert_eq!(seat_to_row_and_col("BFFFBBFRRR"), (70, 7));
-        assert_eq!(seat_to_row_and_col("FFFBBBFRRR"), (14, 7));
-        assert_eq!(seat_to_row_and_col("BBFFBBFRLL"), (102, 4));
+        assert_eq!(seat_to_id("FBFBBFFRLR"), 357);
+        assert_eq!(seat_to_id("BFFFBBFRRR"), 567);
+        assert_eq!(seat_to_id("FFFBBBFRRR"), 119);
+        assert_eq!(seat_to_id("BBFFBBFRLL"), 820);
     }
 }
