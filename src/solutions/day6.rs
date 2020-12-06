87231fd9ab62ca7fb7d34fd02836a67216d42f41
Rickard Andersson <dunklas@gmail.com>
2020-12-06 10:35:24 +0100

    More conciseness for day 6!

--- src/solutions/day6.rs
+++ src/solutions/day6.rs
@@ -7,31 +7,27 @@ pub fn run(input: String) {
 
 fn part_1(input: &String) -> usize {
     input.split("\n\n")
-        .map(|group| {
-            let unique_chars: collections::HashSet<char> = group.chars()
-                .filter(|c| !c.is_whitespace())
-                .collect();
-            unique_chars.len()
-        })
+        .map(|group| group.chars()
+            .filter(|c| !c.is_whitespace())
+            .collect::<collections::HashSet<char>>()
+            .len()
+        )
         .sum()
 }
 
 fn part_2(input: &String) -> usize {
     input.split("\n\n")
-        .map(|group| {
-            let unique_chars: collections::HashSet<char> = group.chars()
-                .filter(|c| !c.is_whitespace())
-                .collect();
-            unique_chars.iter().fold(0, |acc, &c| {
-                if group.matches(c).count() == group.lines().count() {
-                    return acc + 1;
-                }
-                return acc;
+        .map(|group| group.chars()
+            .filter(|c| !c.is_whitespace())
+            .collect::<collections::HashSet<char>>()
+            .iter()
+            .fold(0, |acc, &c| match group.matches(c).count() == group.lines().count() {
+                true => acc + 1,
+                false => acc
             })
-        })
+        )
         .sum()
 }
-
 
 #[cfg(test)]
 mod tests {
